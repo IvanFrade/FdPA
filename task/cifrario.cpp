78ac7e29b202e7939c9838bfb9e9189aae691398
Ivan <fradellin.ivan@gmail.com>
2023-10-15 18:32:32 +0200

    cifrario finale finale davvero

--- task/cifrario.cpp
+++ task/cifrario.cpp
@@ -4,7 +4,7 @@
    Validi solo caratteri alfabetici minuscoli (26), tutti gli altri diventano *
    Attenzione al riporto (x diventa a, y diventa b ecc.)
 
-   Usare solo: dichiarazione, assegnazione, if-else, forse operatore ternario?
+   Usare solo: dichiarazione, assegnazione, if-else
    Nella codifica ASCII i caratteri minuscoli vanno da 97 (a) a 122 (z)
 */
 
@@ -19,17 +19,20 @@ int main() {
     cin >> c1 >> c2 >> c3;
     
     // Se il carattere in input è compreso tra 'a' e 'z' lo shifto di 3, altrimenti lo setto a '*'
-    (c1 >= 'a' && c1 <= 'z') ? c1 += 3 : c1 = '*';
+    if (c1 >= 'a' && c1 <= 'z') c1 += 3;
+    else c1 = '*';
     
-    // Se il carattere originale era x, y o z, incrementato di 3 "sfora" dai caratteri previsti, 
+    // Se il carattere originale era x, y o z, incrementato di 3 else dai caratteri previsti, 
     // quindi riparto dalla 'a' sottraendo 26
     if (c1 > 'z') c1 -= 26; 
 
     // Ripeto per gli altri due char in input
-    (c2 >= 'a' && c2 <= 'z') ? c2 += 3 : c2 = '*';
+    if (c2 >= 'a' && c2 <= 'z') c2 += 3;
+    else c2 = '*';
     if (c2 > 'z') c2 -= 26;
 
-    (c3 >= 'a' && c3 <= 'z') ? c3 += 3 : c3 = '*';
+    if (c3 >= 'a' && c3 <= 'z') c3 += 3;
+    else c3 = '*';
     if (c3 > 'z') c3 -= 26;
 
     cout << "Cifrario di Cesare: ";
