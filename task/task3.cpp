57d18bc2fca808ed5059081195f5d14306e1d8b0
Ivan <fradellin.ivan@gmail.com>
2023-10-27 22:48:47 +0200

    Task 3 finita, da rivedere

--- task/task3.cpp
+++ task/task3.cpp
@@ -8,24 +8,35 @@
 
    Controllo su input operazione, solo un'operazione
 
+   Stampa array: semplice
+
+   Conversione decimale: i positivi semplice, i negativi sono [-7, -1] e corrispondono a [8, 15]
+   quindi posso sottrarre 16 e trovare il negativo senza fare la conversione al contrario
+   -8 caso particolare con errore overflow
+
+   opposto: 
    [0][1][1][1] -> [1][0][0][1]
    0111 = 7 l'opposto e' -7, inverto i bit e ottengo 1000, aggiungo uno e ottengo 1001
    1011 = -5 l'opposto e' 5, inverto i bit e ottengo 0100, aggiungo uno e ottengo 0101
+
 */
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
+const int N_BIT = 8;
+
 int main() {
-    int binario[4], op;
+    int bin[N_BIT], op;
 
-    for (int i = 0; i < 4; i++) {
-        cout << "Inserisci il bit nella posizione " << 3-i << ": ";
-        cin >> binario[i];
+    for (int i = 0; i < N_BIT; i++) {
+        cout << "Inserisci il bit nella posizione " << N_BIT-1-i << ": ";
+        cin >> bin[i];
 
-        while (binario[i] != 0 && binario[i] != 1) {
-            cout << "Errore!\nInserisci il bit nella posizione " << 3-i << ": ";
-            cin >> binario[i];
+        while (bin[i] != 0 && bin[i] != 1) {
+            cout << "Errore!\nInserisci il bit nella posizione " << N_BIT-1-i << ": ";
+            cin >> bin[i];
         }
     }
 
@@ -43,17 +54,52 @@ int main() {
     switch (op) {
         case 0:
             cout << "Numero binario: ";
-            for (int i = 0; i < 4; i++) 
-                cout << binario[i];
+            for (int i = 0; i < N_BIT; i++) 
+                cout << bin[i];
             break;
+
         case 1:
-            // TODO: conversione in decimale e stampa
+        {   
+            int dec = 0;
+            for (int i = N_BIT; i > 0; i--) {
+                dec += bin[i-1] * pow(2, N_BIT-i);
+            }
+
+            if (bin[0])
+                dec -= pow(2, N_BIT);
+
+            cout << "Numero decimale: " << dec;
             break;
+        }
+
         case 2:
-            // TODO: calcolo dell'opposto e stampa
+        {
+            // hardcoded, non funziona con N_BIT != 4
+            if (bin[0] && !bin[1] && !bin[2] && !bin[3]) {
+                cout << "Overflow!";
+                break;
+            }
+
+            bool riporto = true;
+
+            for (int i = N_BIT-1; i >= 0; i--) {
+                ++bin[i] %= 2;
+                if (riporto) {
+                    if (!bin[i])
+                        riporto = false;
+                    
+                    ++bin[i] %= 2;
+                }
+            }
+
+            cout << "Opposto: ";
+            for(int i = 0; i < N_BIT; i++)
+                cout << bin[i];
             break;
+        }
+
         default:
-            cout << "Errore imprevisto"; // Non dovrebbe mai succedere
+            cout << "Errore imprevisto!"; // Non dovrebbe mai succedere
     }
 
     return 0;
