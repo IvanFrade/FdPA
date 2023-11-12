45d5d51749bc89baf1840300257048fbbfde5dd0
Ivan <fradellin.ivan@gmail.com>
2023-11-12 16:58:09 +0100

    Prefisso ok

--- task/task4.cpp
+++ task/task4.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 #include <cstring>
 
 using namespace std;
@@ -9,28 +10,55 @@ int main() {
     char parola1[MAX_LENGTH];
     char parola2[MAX_LENGTH];
 
-    cout << "Inserire la prima parola: ";
-    cin >> parola1;
+    do {
+        cout << "Inserire la prima parola: ";
+        cin >> parola1;
 
-    if (strcmp(parola1, "$") == 0)
-        return 0;
+        if (strcmp(parola1, "$") == 0)
+            break;
 
-    cout << "Inserire la seconda parola: ";
-    cin >> parola2;
+        cout << "Inserire la seconda parola: ";
+        cin >> parola2;
 
-    if (strcmp(parola2, "$") == 0)
-        return 0;
+        if (strcmp(parola2, "$") == 0)
+            break;
 
-    if (strcmp(parola1, parola2)) {
-        cout << "Le due parole sono uguali." << endl << "Reverse: ";
+        if (strcmp(parola1, parola2) == 0) {
+            cout << "Le due parole sono uguali." << endl << "Reverse: ";
 
-        for (int i = strlen(parola1); i > 0; i--)
-            cout << parola1[i - 1];
+            for (int i = strlen(parola1); i > 0; i--)
+                cout << parola1[i - 1];
 
-    }
-    else if (strlen(parola1) != strlen(parola2)) {
-        // TODO: finire
-    }
+            cout << endl << endl;
+        }
+
+        else {
+            if (strlen(parola1) > strlen(parola2)) {
+                char tmp[100];
+                strcpy(tmp, parola1);
+                strcpy(parola1, parola2);
+                strcpy(parola2, tmp);
+            }
+
+            if (strncmp(parola1, parola2, strlen(parola1)) == 0)
+                cout << "La parola " << parola1 << " e' prefisso di " << parola2 << endl << endl;
+            else {
+                for (int i = 0; i < strlen(parola1) / 2; i++) {
+                    char tmp = parola1[i];
+                    parola1[i] = parola1[strlen(parola1) - i - 1];
+                    parola1[strlen(parola1) - i - 1] = tmp;
+                }
+
+                for (int i = 0; i < strlen(parola1); i++) 
+                    parola2[i] = parola2[strlen(parola2) - i - 1];
+                    if (strncmp(parola1, parola2, strlen(parola1)) == 0)
+                        cout << "La parola " << parola1 << " e' suffisso di " << parola2 << endl << endl;
+            }
+                
+            cout << endl << endl;
+        }
+
+    } while (1);
 
     return 0;   
 }
