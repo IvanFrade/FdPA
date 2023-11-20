fc9bb1946f2826a3324d1a208ecf402822ce45c6
Ivan <fradellin.ivan@gmail.com>
2023-11-20 20:00:59 +0100

    Aggiunti check all'input + bugfix

--- task/task5.cpp
+++ task/task5.cpp
@@ -3,17 +3,19 @@
 
 using namespace std;
 
+#define MAX_CONTATTI 100    // Max contatti in rubrica
+
 struct persona { 
-    char nome[64];
-    char cognome[64];
+    char nome[65];
+    char cognome[65];
     char numero[11]; // 10 cifre + carattere di terminazione
 };
 
 int main() {
-    persona rubrica[100];   // Rubrica di contatti
-    int nContatti = 0;      // Indice della rubrica
-    int op;                 // Operazione dell'utente
-    bool found = 0;         // Flag contatto trovato
+    persona rubrica[MAX_CONTATTI];  // Rubrica di contatti
+    int nContatti = 0;              // Indice della rubrica
+    int op;                         // Operazione dell'utente
+    bool found = 0;                 // Flag contatto trovato
 
     cout << "*** Rubrica ***" << endl;
 
@@ -24,6 +26,17 @@ int main() {
         cout << "\t3 - Cancella contatto" << endl;
         cout << "\t4 - Esci" << endl;
         cin >> op;
+
+        // Check se l'utente inserisce lettere
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore(9999, '\n');
+
+            cout << "Input non valido!" << endl;
+
+            continue;
+        }
+
         cin.ignore(); // Ignora il \n alla fine
 
         switch (op)
@@ -42,19 +55,35 @@ int main() {
 
         case 2: {   // Aggiungi contatto
             // Controllo se la rubrica e' piena
-            if (nContatti > 2) {
+            if (nContatti >= MAX_CONTATTI) {
                 cout << "La rubrica e' piena! Elimina uno o piu' contatti e riprova." << endl;
                 break;
             }
 
-            persona p;
+            persona p;  // Persona da aggiungere alla rubrica
             found = 0;
 
             cout << "Inserisci il nome del contatto da aggiungere: ";
-            cin.getline(p.nome, 64);
+            cin.getline(p.nome, 65);
+
+            while (cin.fail()) {
+                cin.clear();
+                cin.ignore(9999, '\n');
+
+                cout << "Nome troppo lungo! Puoi inserire max 64 caratteri. Riprova: ";
+                cin.getline(p.nome, 65);
+            }
             
             cout << "Inserisci il cognome del contatto da aggiungere: ";
-            cin.getline(p.cognome, 64);
+            cin.getline(p.cognome, 65);
+
+            while (cin.fail()) {
+                cin.clear();
+                cin.ignore(9999, '\n');
+
+                cout << "Cognome troppo lungo! Puoi inserire max 64 caratteri. Riprova: ";
+                cin.getline(p.cognome, 65);
+            }
 
             // Non sono consentiti doppioni
             for (int i = 0; i < nContatti; i++)
@@ -64,8 +93,47 @@ int main() {
                 }
             
             if (!found) {
-                cout << "Inserisci il numero del contatto da aggiungere: ";
-                cin.getline(p.numero, 64);
+                bool numValido; // Flag se il numero e' valido
+
+                do {
+                    cout << "Inserisci il numero del contatto da aggiungere: ";
+                    cin.getline(p.numero, 11);
+
+                    numValido = 1;
+
+                    // Check sulla lunghezza
+                    if (cin.fail()) {
+                        numValido = 0;
+
+                        cin.clear();
+                        cin.ignore(9999, '\n');
+
+                        cout << "Input non valido! Lunghezza max 10 caratteri." << endl;
+
+                        continue;
+                    }
+
+                    // Check che l'utente abbia inserito solo cifre
+                    int j = 0;
+                    
+                    while (p.numero[j] != '\0') {
+                        if (p.numero[j] < 48 || p.numero[j] > 57) {     // Nella tabella ASCII numeri compresi tra 48 e 57
+                            cout << "Il numero di telefono puo' contenere solo cifre!" << endl;
+                            numValido = 0;
+
+                            break;
+                        }
+
+                        j++;
+                    }
+
+                    // Check che il numero abbia almeno 9 cifre 
+                    if (j < 9) {
+                        cout << "Numero troppo corto! Deve avere almeno 9 cifre." << endl;
+                        numValido = 0;
+                    }
+
+                } while (!numValido);
 
                 rubrica[nContatti] = p; 
                 nContatti++;
@@ -77,15 +145,32 @@ int main() {
         }
 
         case 3: {   // Elimina contatto
-            char nome[64], cognome[64];
+            char nome[65], cognome[65];     // Variabili di appoggio per la ricerca
             found = 0;
+
             cout << "Inserisci il nome del contatto da cancellare: ";
             cin.getline(nome, 64);
+
+            while (cin.fail()) {
+                cin.clear();
+                cin.ignore(9999, '\n');
+
+                cout << "Nome troppo lungo! Puoi inserire max 64 caratteri. Riprova: ";
+                cin.getline(nome, 64);
+            }
 
             cout << "Inserisci il cognome del contatto da cancellare: ";
             cin.getline(cognome, 64);
 
-            // Ricerca lineare del contatto da elimiare
+            while (cin.fail()) {
+                cin.clear();
+                cin.ignore(9999, '\n');
+
+                cout << "Cognome troppo lungo! Puoi inserire max 64 caratteri. Riprova: ";
+                cin.getline(cognome, 64);
+            }
+
+            // Ricerca del contatto da elimiare
             for (int i = 0; i < nContatti; i++) {
                 if (strcmp(nome, rubrica[i].nome) == 0 && strcmp(cognome, rubrica[i].cognome) == 0) {
                     
