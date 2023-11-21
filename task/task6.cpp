035bea93dd2c7774b78497dc2e52c45eaffb5de1
Ivan <fradellin.ivan@gmail.com>
2023-11-21 21:35:39 +0100

    boh

---
+++ task/task6.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int* separa(char s[100], char alpha[100], char num[100]) {
+
+}
+
+int main() {
+    char str[100];
+    char SAlpha[100], SNum[100];
+
+    int contatore[2] = {0, 0};
+
+    cout << "Inserisc una stringa: ";
+    cin.getline(str, 99);
+
+    contatore = separa(str, SAlpha, SNum)
+
+    return 0;
+}
