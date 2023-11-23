0d6465dab81f2f01891423a71dd43c1c047fec82
Ivan <fradellin.ivan@gmail.com>
2023-11-23 22:33:59 +0100

    Comments

--- task/task6.cpp
+++ task/task6.cpp
@@ -3,26 +3,30 @@
 
 using namespace std;
 
+// Contatore lettere e cifre
 struct Contatore {
     int lettere;
     int cifre;
 };
 
+// Riceve in input una stringa, estrae cifre e lettere e le inserisce 
+// in due stringhe separate
+// Restituisce il numero di lettere e cifre trovate nella stringa input
 Contatore separa(char* s, char* alpha, char* num) {
     Contatore contatore = {0, 0};
 
     for (int i = 0; i < strlen(s); i++) {
-        if (s[i] >= 48 && s[i] <= 57 ) {
+        if (s[i] >= 48 && s[i] <= 57 ) {            // Cifre
 
             num[contatore.cifre] = s[i];
             contatore.cifre++;
         }
 
-        else if ((s[i] >= 65 && s[i] <= 90) || 
+        else if ((s[i] >= 65 && s[i] <= 90) ||      // Lettere
             (s[i] >= 97 && s[i] <= 122)) {
 
-                alpha[contatore.lettere] = s[i];
-                contatore.lettere++;
+            alpha[contatore.lettere] = s[i];
+            contatore.lettere++;
         }
 
     }
@@ -32,9 +36,10 @@ Contatore separa(char* s, char* alpha, char* num) {
 
 
 int main() {
-    char str[101];
-    char SAlpha[100], SNum[100];
-    Contatore contatore;
+    char str[101];                  // Stringa input
+    char SAlpha[100];               // Lettere nell'input
+    char SNum[100];                 // Cifre nell'input
+    Contatore contatore;            // Contatore cifre e lettere nell'input
 
     cout << "Inserisci una stringa: ";
     cin.getline(str, 101);
@@ -49,6 +54,7 @@ int main() {
 
     contatore = separa(str, SAlpha, SNum);
 
+    // Stampa
     cout << "Caratteri alfabetici" << endl;
 
     for (int i = 0; i < contatore.lettere; i++) {
