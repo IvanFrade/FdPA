4a54752c15e61ae6de44e391e88a54b9fa78de7b
Ivan <fradellin.ivan@gmail.com>
2023-11-26 16:59:13 +0100

    more refactor, done tho im turning it in

--- task/task6.cpp
+++ task/task6.cpp
@@ -36,26 +36,26 @@ void separa(char s[], char alpha[], char num[], int& nLettere, int& nCifre) {
     }
 }
 
-// Stampa delle due substring
+// Stampa singola stringa aggiungendo uno spazio tra ogni carattere
+void stampaStringa(char str[], int len) {
+    for (int i = 0; i < len; i++)
+        cout << str[i] << " ";
+}
+
+// Stampa del risultato
 void stampa(char lettere[], char cifre[], int nLettere, int nCifre) {
     if (nLettere <= 0)
         cout << "La stringa non contiene caratteri alfabetici";
     else {
         cout << "Caratteri alfabetici" << endl;
-
-        for (int i = 0; i < nLettere; i++) {
-            cout << lettere[i] << " ";
-        }
+        stampaStringa(lettere, nLettere);
     }
 
     if (nCifre <= 0)
         cout << endl << "La stringa non contiene caratteri numerici" << endl;
     else {
         cout << endl << "Caratteri numerici" << endl;
-
-        for (int i = 0; i < nCifre; i++) {
-            cout << cifre[i] << " ";
-        }
+        stampaStringa(cifre, nCifre);
     }
 }
 
@@ -66,11 +66,8 @@ int main() {
     int nLettere = 0, nCifre = 0;   // Contatore cifre e lettere nell'input
 
     leggiStringa(str);
-
     separa(str, SAlpha, SNum, nLettere, nCifre);
-
     stampa(SAlpha, SNum, nLettere, nCifre);
-    
 
     return 0;
 }
