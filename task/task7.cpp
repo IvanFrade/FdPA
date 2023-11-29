92b8567fa3869a4e1871dc8922f5dcf02b8e8d98
Ivan <fradellin.ivan@gmail.com>
2023-11-29 22:07:53 +0100

    Iniziata task 7

---
+++ task/task7.cpp
@@ -0,0 +1,20 @@
+/**
+ * leggere da tastiera interi positivi fino al primo negativo inserito
+ * memorizzarli in un array A di puntatori a interi max. 1000 elementi
+ * per ogni x, se x non e' in A, variabile dinamica che memorizza x e aggiunta del puntatore alla variabile dinamica
+ * se x e' gia in A, aggiunge ad A il puntatore alla variabile contenente x, senza allocare ancora
+ * 
+ * stampare il numero di variabili allocate e A
+ * 
+ * No altre funzioni
+ * deadline 5/12 mezzanotte
+*/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+
+    return 0;
+}
